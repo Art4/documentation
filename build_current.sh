23307c5dfa569c39f5f40844abd4d7ee0ab83f3b
Art4 <art4@wlabs.de>
2018-03-28 23:21:33 +0200

    Move secure GH_TOKEN to travis.org website

--- build_current.sh
+++ build_current.sh
@@ -4,12 +4,10 @@
 # Fill this out with the correct org/repo
 ORG=Art4
 REPO=documentation
-# This probably should match an email for one of your users.
-EMAIL=art4@wlabs.de
 
 set -e
 
-# Clone the gh-pages branch outside of the repo and cd into it.
+# Clone the master branch outside of the repo and cd into it.
 cd ..
 git clone -b master "https://$GH_TOKEN@github.com/$ORG/$REPO.git" master-branch
 cd master-branch
@@ -17,18 +15,23 @@ cd master-branch
 # Update git configuration so I can push.
 if [ "$1" != "dry" ]; then
     # Update git config.
-    git config user.name "Travis Builder"
-    git config user.email "$EMAIL"
+    git config user.name "Deployment Bot"
+    git config user.email "deploy@travis-ci.org"
 fi
 
+echo "removing current folder"
 rm -r docs/current/
+
+echo "copy new files to current folder"
 # Copy in the HTML.  You may want to change this with your documentation path.
 cp -r ../$REPO/_build/html/ ./docs/current
 
 # Add and commit changes.
+echo "create commit"
 git add -A .
 git commit -m "[ci skip] Build current version from $COMMIT."
 if [ "$1" != "dry" ]; then
+    echo "push to Github repo"
     # -q is very important, otherwise you leak your GH_TOKEN
     git push -q origin master
 fi
